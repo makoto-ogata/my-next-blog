258d5383d8fb2787c4e4a36af5ae8c13eed943c4
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-09-17 18:47:15 +0900

    create about and blog

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
-import Header from 'components/header'
-import Footer from 'components/footer'
 import Hero from 'components/hero'
-import Layout from 'components/layout'
 
 export default function Home() {
-  return(
-      <>
-          <Layout>
-              <Hero />
-          </Layout>
-      </>
-  )
+  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" />
 }
