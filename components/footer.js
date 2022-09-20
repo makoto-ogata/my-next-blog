990b091212d414a1db5533f30bac70984e4c3f68
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-09-20 23:11:31 +0900

    add container to header and footer

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,16 @@
 import Logo from './logo'
+import Container from 'components/container'
 import styles from 'styles/footer.module.css'
 
 export default function Footer() {
     return (
         <footer className={styles.wrapper}>
-            <div className={styles.flexContainer}>
-                <Logo />
-                [ソーシャル]
-            </div>
+            <Container>
+                <div className={styles.flexContainer}>
+                    <Logo />
+                    [ソーシャル]
+                </div>
+            </Container>
         </footer>
     )
 }
--- components/header.js
+++ components/header.js
@@ -1,12 +1,17 @@
-import Logo from './logo'
-import Nav from './nav'
+import Logo from 'components/logo'
+import Nav from 'components/nav'
+import Container from 'components/container'
 import styles from 'styles/header.module.css'
 
 export default function Header() {
     return(
-        <header className={styles.flexContainer}>
-            <Logo boxOn />
-            <Nav />
+        <header>
+            <Container large>
+                <div className={styles.flexContainer}>
+                    <Logo boxOn />
+                    <Nav />
+                </div>
+            </Container>
         </header>
     )
 }
