951a843cb94e1b9d12b2fe8e798c0b92c09dc17e
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-09-20 22:50:35 +0900

    create header.module.css and add flexContainer to hero.module.css

--- components/header.js
+++ components/header.js
@@ -1,9 +1,10 @@
 import Logo from './logo'
 import Nav from './nav'
+import styles from 'styles/header.module.css'
 
 export default function Header() {
     return(
-        <header>
+        <header className={styles.flexContainer}>
             <Logo boxOn />
             <Nav />
         </header>
