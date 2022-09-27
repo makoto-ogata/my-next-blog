31cc55a622f08426d8dfc2aeb9012159d2f94a76
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-09-27 23:18:50 +0900

    add url and favicon image

--- components/meta.js
+++ components/meta.js
@@ -1,16 +1,21 @@
 import Head from 'next/head'
 import { siteMeta } from 'lib/constants'
+import {useRouter} from 'next/router'
 const { siteTitle, siteDesc, siteUrl, siteLocal, siteType, siteIcon } = siteMeta
 
 export default function Meta({pageTitle, pageDesc}){
     const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
     const desc = pageDesc ?? siteDesc
+    const router = useRouter()
+    const url = `${siteUrl}${router.asPath}`
     return (
         <Head>
             <title>{title}</title>
             <meta property="og:title" content={title} />
             <meta name="description" content={desc} />
             <meta property="og:description" content={desc} />
+            <link rel="canonical" href={url} />
+            <meta property="og:url" content={url} />
         </Head>
     )
 }
