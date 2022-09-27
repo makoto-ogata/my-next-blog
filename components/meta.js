2474d70d118bfdd170e570fcf1eb1ca3075bfef5
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-09-27 23:12:24 +0900

    add description

--- components/meta.js
+++ components/meta.js
@@ -2,12 +2,15 @@ import Head from 'next/head'
 import { siteMeta } from 'lib/constants'
 const { siteTitle, siteDesc, siteUrl, siteLocal, siteType, siteIcon } = siteMeta
 
-export default function Meta({pageTitle}){
+export default function Meta({pageTitle, pageDesc}){
     const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+    const desc = pageDesc ?? siteDesc
     return (
         <Head>
             <title>{title}</title>
             <meta property="og:title" content={title} />
+            <meta name="description" content={desc} />
+            <meta property="og:description" content={desc} />
         </Head>
     )
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,7 +5,7 @@ import Container from 'components/container'
 export default function Blog(){
     return (
         <Container>
-            <Meta pageTitle="ブログ" />
+            <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
             <Hero title="Blog" subtitle="Recent Posts" />
         </Container>
     )
